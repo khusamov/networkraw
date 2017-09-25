8697c0296c531e54e2d5a513fc8eab08911aa5dc
Sukhrob Khusamov <khusamov@yandex.ru>
2017-09-25 09:51:00 +0300

    Добавлена заготовка для перевода расширения на ExtJS

--- devtools.js
+++ devtools.js
@@ -1,4 +1,4 @@
 'use strict';
 //Add extenstion panel to the DevTools
 //NOTE: The extenstion panel load ONLY when you manually hit them first!
-chrome.devtools.panels.create("Network RAW", "", "panel.html");
+chrome.devtools.panels.create("Network RAW", "", "build/production/NetworkRaw/index.html");
